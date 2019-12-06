5c45c6bda36ffaefe3a29f71e01481af29fd246e
Tai <thaivinhtai@gmail.com>
2019-12-06 23:29:11 +0700

    update test glance script

--- scripts/test_glance.sh
+++ scripts/test_glance.sh
@@ -6,10 +6,7 @@ set -e
 
 echo "Registering Cirros IMAGE for GLANCE"
 
-wget -c http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
-
 openstack image create "cirros" \
-    --id 16c4bc2a-1143-4b46-b9ed-87768cc82223 \
     --file cirros-0.4.0-x86_64-disk.img \
     --disk-format qcow2 \
     --container-format bare \
